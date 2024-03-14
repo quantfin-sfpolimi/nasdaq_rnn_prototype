179e4cc61b416bc7014dac12db9d37f2d60e1d2b
giutala <giuliatala32@gmail.com>
2024-03-14 15:17:41 +0100

    fixing some bugs - gt

--- load_stocks.py
+++ load_stocks.py
@@ -1,4 +1,4 @@
-import pickle5 as pickle
+import pickle
 
 with open("stocks_prices_dataframe.pkl", "rb") as f:
     stocks_prices = pickle.load(f)
--- pickling_stocks_prices.py
+++ pickling_stocks_prices.py
@@ -1,44 +1,60 @@
 import yahoo_fin.stock_info as si
 import pandas as pd
 import datetime
-import pickle5 as pickle
+import pickle
 
-# get NASDAQ ticker
-tables = pd.read_html('https://en.wikipedia.org/wiki/Nasdaq-100')
-df = tables[4]
+#make function to get list of tickers
+def get_list_tickers(link):
+  # get NASDAQ ticker
+  tables = pd.read_html(link)
+  df = tables[4]
+  # clean df
+  df.drop(['Company','GICS Sector', 'GICS Sub-Industry'], axis=1, inplace=True)
+  # put data in a list
+  tickers = df['Ticker'].values.tolist()
+  return tickers
 
-# clean df
-df.drop(['Company','GICS Sector', 'GICS Sub-Industry'], axis=1, inplace=True)
+#function to get data from a timeframe and load it into a dataframe
+def loaded_df(years, tickers):
+  stocks_dict = {}
+  # set the period
+  time_window = 365*years
+  start_date = datetime.datetime.now() - datetime.timedelta(time_window)
+  end_date = datetime.datetime.now()
 
-# put data in a list
-tickers = df['Ticker'].values.tolist()
+  # get data and load into a dataframe
+  for i, ticker in enumerate(tickers):
+    print('Getting {} ({}/{})'.format(ticker, i, len(tickers)))
+    prices = si.get_data(ticker, start_date = start_date, end_date = end_date)
+    stocks_dict[ticker] = prices['adjclose']
+  
+  stocks_prices = pd.DataFrame.from_dict(stocks_dict)
+  return stocks_prices
 
-# create an empty data frame
-stocks_prices = pd.DataFrame()
+#dataframe to drop collumns with too much dirty date (under certain given percentage)
+def clean_df(percentage, tickers, stocks_prices):
+  if percentage > 1: 
+    percentage = percentage/100
+  # check NaN values
+  for ticker in tickers:
+    nan_values = stocks_prices[ticker].isnull().values.any()
+    if nan_values == True:
+      # count NaN values
+      count_nan = stocks_prices[ticker].isnull().sum()
+      # remove NaN values
+      if count_nan > (len(stocks_prices)*percentage):
+        stocks_prices.drop(ticker, axis=1, inplace=True)
+  # clean remaining NaN
+  stocks_prices.dropna(inplace = True)
+  return stocks_prices
 
-# set the period
-time_window = 365*5
-start_date = datetime.datetime.now() - datetime.timedelta(time_window)
-end_date = datetime.datetime.now()
 
-# get data and load into a dataframe
-for ticker in tickers:
-  stock_prices = si.get_data(ticker, start_date = start_date, end_date = end_date)
-  stocks_prices[ticker] = stock_prices['adjclose']
-
-# check NaN values
-for ticker in tickers:
-  nan_values = stocks_prices[ticker].isnull().values.any()
-  if nan_values == True:
-    # count NaN values
-    count_nan = stocks_prices[ticker].isnull().sum()
-    # remove NaN values
-    if count_nan > (len(stocks_prices)*0.1):
-      stocks_prices.drop(ticker, axis=1, inplace=True)
-
-# clean remaining NaN
-stocks_prices.dropna(inplace = True)
-
+#example of usage
+tickers = get_list_tickers('https://en.wikipedia.org/wiki/Nasdaq-100')
+stocks_prices = loaded_df(5, tickers=tickers)
+stocks_prices = clean_df(0.1, tickers=tickers, stocks_prices=stocks_prices)
 # saving stocks_prices in a pickle file
 with open("stocks_prices_dataframe.pkl", "wb") as f:
     pickle.dump(stocks_prices, f)
+    
+    
