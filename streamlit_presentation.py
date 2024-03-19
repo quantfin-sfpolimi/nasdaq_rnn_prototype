420fa8347b7ac5287450ac75e53db34c440686fc
giutala <giuliatala32@gmail.com>
2024-03-19 14:00:17 +0100

    working on it

--- streamlit_presentation.py
+++ streamlit_presentation.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import pandas_profiling
-from streamlit_pandas_profiling import st_profile_report
 
 with open("stocks_prices_dataframe.pkl", "rb") as f:
     stocks_prices = pickle.load(f)
@@ -13,5 +11,4 @@ st.write('The first step was to preprocess the data, which you can see in the fo
 st.line_chart(stocks_prices.iloc[:, :10], width=1)
 st.write('But what do we do with the data? I\'m glad you asked :)')
 st.write('Apart from drawing pretty graphs, we also worked with the data!')
-pr = stocks_prices.profile_report()
-st_profile_report(pr)
+st.write('come abbiamo lavorato (prospettiva del modelo finale), l\'output, qualche grafico carino, grafico della loss function e delle epochs')
